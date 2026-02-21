efc74a342639e9e2fb2e95ba2041320a8ae69fc8
Cornet Jean <jlcornet878@gmail.com>
2026-02-21 16:15:31 +0000

    File changes

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -107,10 +107,12 @@ export default function Header({ user }) {
                       Loisirs et Intérêts
                     </DropdownMenuItem>
                   </Link>
-                  <DropdownMenuItem className="py-3 px-4 cursor-pointer hover:bg-gray-50 rounded text-gray-700">
-                    <HelpCircle className="w-4 h-4 mr-3 text-gray-400" />
-                    Questions Sur Votre Personnalité
-                  </DropdownMenuItem>
+                  <Link to={createPageUrl('PersonalityQuestions')}>
+                    <DropdownMenuItem className="py-3 px-4 cursor-pointer hover:bg-gray-50 rounded text-gray-700">
+                      <HelpCircle className="w-4 h-4 mr-3 text-gray-400" />
+                      Questions Sur Votre Personnalité
+                    </DropdownMenuItem>
+                  </Link>
                   <DropdownMenuItem className="py-3 px-4 cursor-pointer hover:bg-gray-50 rounded text-gray-700">
                     <CheckCircle className="w-4 h-4 mr-3 text-gray-400" />
                     Vérifiez profil
