a46357d454cb20ff4c18c65da6eb49719885834d
Cornet Jean <jlcornet878@gmail.com>
2026-03-01 09:59:50 +0000

    File changes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,6 +6,7 @@ import { Input } from '@/components/ui/input';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Heart, Eye, EyeOff, Facebook, Twitter, Instagram, Youtube, Lock, ShieldCheck, X, AlertCircle } from 'lucide-react';
+import Sitemap from '@/components/layout/Sitemap';
 import { isWithinInterval } from 'date-fns';
 import { createPageUrl } from '@/utils';
 
